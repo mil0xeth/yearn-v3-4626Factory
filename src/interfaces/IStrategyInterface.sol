a6cd8cadb054ced9310c7c489613094d43cb54f7
Schlag <89420541+Schlagonia@users.noreply.github.com>
2023-08-14 10:54:25 -0600

    feat: rewards switch (#1)
    * feat: claim rewards switch
    
    * test: remove oracel test

--- src/interfaces/IStrategyInterface.sol
+++ src/interfaces/IStrategyInterface.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.18;
 import {IStrategy} from "@tokenized-strategy/interfaces/IStrategy.sol";
 
 interface IStrategyInterface is IStrategy {
+    function claimRewards() external view returns (bool);
+
     function setUniFees(uint24 _compToEth, uint24 _ethToAsset) external;
 
     function setMinAmountToSell(uint256 _minAmountToSell) external;
+
+    function setClaimRewards(bool _claimRewards) external;
 }
